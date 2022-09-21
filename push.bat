b24e14ff58c4b852ec31f544cb7ca29ca1cc2906
MericaTudias <mat6524@students.uc-bcf.edu.ph>
2022-09-21 15:33:29 +0800

    CV

---
+++ push.bat
@@ -0,0 +1,7 @@
+@echo off
+git remote set-url origin https://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github.com/MericaTudias/https://github.com/MericaTudias/CIT42G.git
+pause
+git add .
+git commit-m "Auto upload"
+git push
+pause 
